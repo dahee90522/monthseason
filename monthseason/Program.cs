7b1e3e7a5a764e2f27c87cc0fa345799ba07bb77
dahee90522 <s2018s40@e-mirim.hs.kr>
2020-05-06 16:55:01 +0900

    문제 6

--- monthseason/Program.cs
+++ monthseason/Program.cs
@@ -10,7 +10,41 @@ namespace monthseason
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("월 입력 : ");
+            var input = Console.ReadLine();
+
+            if (input.Contains("월"))
+            {
+                input = input.Substring(0, 1);
+            }
+
+            switch (int.Parse(input))
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("겨울");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("봄");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("여름");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("가을");
+                    break;
+                default:
+                    Console.WriteLine("잘못입력");
+                    break;
+
+            }
 
         }
     }
